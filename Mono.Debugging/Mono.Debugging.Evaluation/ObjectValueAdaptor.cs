4da7c667c1eea5cd4ed651bce05d87e1360857af
Jeffrey Stedfast <jeff@xamarin.com>
2013-03-01 19:45:40 -0500

    Don't ever call the .cctor on a type if it's already been initialized
    Fixes bug #10832

--- Mono.Debugging/Mono.Debugging.Evaluation/ObjectValueAdaptor.cs
+++ Mono.Debugging/Mono.Debugging.Evaluation/ObjectValueAdaptor.cs
@@ -379,9 +379,6 @@ namespace Mono.Debugging.Evaluation
 			if (t == null || IsTypeLoaded (ctx, t))
 				return t;
 
-			if (!ctx.Options.AllowTargetInvoke)
-				return null;
-
 			if (ForceLoadType (ctx, t))
 				return t;
 
